06f525a14158890e6d0a63a98e7ec2f2ae04f680
jirin <jirinovy@outlook.com>
2021-06-10 15:48:59 +0100

    Add/change alien_shot.py docstrings.

--- alien_shot.py
+++ alien_shot.py
@@ -9,6 +9,12 @@ class AlienShot(pygame.sprite.Sprite):
     player_explosion_sound = pygame.mixer.Sound(PLAYER_EXPLOSION_SOUND)
 
     def __init__(self, shot_path, position, shot_size=20, shot_color=WHITE):
+        """
+        :param shot_path: string
+        :param position: pygame.Rect
+        :param shot_size: int
+        :param shot_color: (int, int, int)
+        """
         super().__init__()
         try:
             # choose one of two types of alien shots
@@ -37,10 +43,12 @@ class AlienShot(pygame.sprite.Sprite):
         self.penetration_counter = 0
 
     def move(self):
+        """Moves alien."""
         self.update()
         self.corner.move_ip(self.direction_x * self.speed, self.direction_y * self.speed)
 
     def update(self):
+        """Animates alien shot."""
         # update only every 6 calls
         if self.update_count % ALIEN_SHOT_UPDATE_SPEED == 0:
             if self.current < len(self.sprites) - 1:
@@ -55,9 +63,11 @@ class AlienShot(pygame.sprite.Sprite):
         self.update_count += 1
 
     def player_hit_sound(self):
+        """Plays player hit sound."""
         self.player_explosion_sound.play()
 
     def out_of_screen(self):
+        """Initiates shot destruction if it has left the screen."""
         # If shot gets out of screen area
         if SCREEN_HEIGHT + ALIEN_SHOT_EXPLOSION_HEIGHT // 2 <= self.corner.bottom:
             if self.destruct_start_time is None:
@@ -65,18 +75,28 @@ class AlienShot(pygame.sprite.Sprite):
                 self.init_destruction(explosion_sprite=ALIEN_SHOT_EXPLOSION_GREEN)
 
     def init_destruction(self, explosion_sprite=ALIEN_SHOT_EXPLOSION):
+        """
+        Initiates shot destruction and changes shot sprite for shot explosion sprite.
+        :param explosion_sprite: str
+        :return:
+        """
         # show alien shot explosion
         self.surface = pygame.image.load(explosion_sprite).convert_alpha()
         # get current time in milliseconds
         self.destruct_start_time = pygame.time.get_ticks()
 
     def destroy(self):
+        """Destroys the alien shot."""
         # reset destruction start time
         self.destruct_start_time = None
         # destroy shot
         self.kill()
 
     def update_destroyed(self):
+        """
+        Checks whether DESTRUCTION_TIME has elapsed. If it has, calls self.destroy method on the alien shot.
+        :return: bool
+        """
         # check whether alien shot is to be destroyed
         if self.destruct_start_time and (pygame.time.get_ticks() - self.destruct_start_time >= DESTRUCTION_TIME):
             self.destroy()
@@ -84,6 +104,12 @@ class AlienShot(pygame.sprite.Sprite):
         return False
 
     def wall_collision(self, wall_group_list):
+        """
+        Detects alien shot collision with walls in wall_group_list.
+        Initiates shot destruction after ALIEN_SHOT_PENETRATION of wall objects is killed.
+        :param wall_group_list: list[list[Wall]]
+        :return:
+        """
         for wall_group in wall_group_list:
             for wall_piece in wall_group:
                 if self.corner.colliderect(wall_piece.corner):
@@ -101,6 +127,12 @@ class AlienShot(pygame.sprite.Sprite):
                         self.penetration_counter += 1
 
     def spaceship_collision(self, spaceship, scoreboard):
+        """
+        Detects collision with spaceship. Initiates alien shot destruction if spaceship is hit.
+        :param spaceship: Spaceship
+        :param scoreboard: Scoreboard
+        :return: bool
+        """
         if self.corner.colliderect(spaceship.corner) and spaceship.destruct_start_time is None:
             if self.destruct_start_time is None:
                 # initiate shot destruction
@@ -117,6 +149,11 @@ class AlienShot(pygame.sprite.Sprite):
         return False
 
     def line_collision(self, green_line):
+        """
+        Detects alien shot collision with the green HUD line on the bottom of the screen.
+        :param green_line: list[dict[pygame.Surface, pygame.Rect]]
+        :return:
+        """
         for i in range(len(green_line)):
             if green_line[i] is not None:
                 if self.corner.colliderect(green_line[i]["corner"]) and self.destruct_start_time is None:
